1a1a42de95317b55eb9dc556f77c2ad27ef54be5
Kevin Mensah <48464499+mamawpsg16@users.noreply.github.com>
2024-05-22 17:20:01 +0800

    Add Button Event Listener Test

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log('UPDATED LOG');
+document.addEventListener("DOMContentLoaded", () =>{
+    const btnTest = document.querySelector('#btnTest');
+    btnTest.addEventListener('click',() =>{
+        alert('Clicked');
+    });
+})
